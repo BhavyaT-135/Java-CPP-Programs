798c2052e690b1c61158fb508e921e3155d8edaf
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-06-01 19:01:04 +0530

    Binary Search Questions

---
+++ src/com/array/PositionInInfiniteArr.java
@@ -0,0 +1,49 @@
+package com.array;
+
+public class PositionInInfiniteArr {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println(findingRange(arr, 3));
+    }
+
+    static int findingRange(int[] arr, int target) {
+        // first find the range
+        // first start with a box of size 2
+        int start = 0;
+        int end = 1;
+        int ans = -1;
+
+        if (arr[0] == target) {
+            return 0;
+        }
+
+        if (arr[1] == target) {
+            return 1;
+        }
+
+        // condition for the target to lie in the range
+        while (target > arr[end]) {
+            int newStart = end + 1;
+            end = end + ((end - start + 1) * 2);
+            ans = searchRange(arr, target, newStart, end);
+        }
+        return ans;
+    }
+
+    static int searchRange(int[] nums, int target, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
--- src/com/array/Problem34.java
+++ src/com/array/Problem34.java
@@ -2,6 +2,38 @@ package com.array;
 
 public class Problem34 {
     public static void main(String[] args) {
-        
+        int[] arr = { 1, 1, 2, 2, 2, 2, 3 };
+        int[] ans = searchRange(arr, 2);
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+
+    static public int[] searchRange(int[] nums, int target) {
+        int[] ans = { -1, -1 };
+        if (nums.length == 0) {
+            return ans;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                int left = mid;
+                int right = mid;
+                while (left >= 0 && nums[left] == target) {
+                    left--;
+                }
+                while (right < nums.length && nums[right] == target) {
+                    right++;
+                }
+                ans[0] = left + 1;
+                ans[1] = right - 1;
+                return ans;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return ans;
     }
 }

a65102e32153f63775009955db2dc71939a1f03b
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-09-11 16:22:52 +0530

    Rotation Search with Duplicate Elements

--- src/com/array/RotatedSortSearch.java
+++ src/com/array/RotatedSortSearch.java
@@ -2,9 +2,9 @@ package com.array;
 
 public class RotatedSortSearch {
     public static void main(String[] args) {
-        int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
-        System.out.println(findPivot(arr));
-        System.out.println(search(arr, 6));
+        int[] arr = { 2, 9, 2, 2, 2 };
+        System.out.println(findPivotWithDuplicates(arr));
+        System.out.println(search(arr, 9));
     }
 
     static int search(int[] nums, int target) {
@@ -63,4 +63,45 @@ public class RotatedSortSearch {
         }
         return -1;
     }
+
+    static int findPivotWithDuplicates(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            // if elements at middle, start, end are same then just skip the duplicates
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                // skip the duplicates
+
+                // NOTE: what if these elements at start and end were the pivots??
+                // Check if start is pivot
+                if (arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+                // Check if end is pivot
+                if (arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted, so pivot should be on right side
+            else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            }
+            // right side is sorted, so pivot should be on left side
+            else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
+ 

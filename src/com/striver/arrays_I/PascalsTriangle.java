dba550dc71d4888620fc4e51c21e890b6942d79e
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-10-17 16:45:04 +0530

    Pascals Triangle

---
+++ src/com/striver/arrays_I/PascalsTriangle.java
@@ -0,0 +1,43 @@
+package com.striver.arrays_I;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> row, pre = null;
+        for (int i = 0; i < numRows; i++) {
+            row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; j++)
+                if (j == 0 || j == i)
+                    row.add(1);
+                else
+                    row.add(pre.get(j - 1) + pre.get(j));
+            pre = row;
+            res.add(row);
+        }
+        return res;
+    }
+}

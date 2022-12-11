cd479d247a4b36df97b676ac42958c7a1b369e6f
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-12-11 23:15:31 +0530

    Striver - MergedSortedArray

---
+++ src/com/striver/arrays_I/MergedSortedArrays.java
@@ -0,0 +1,31 @@
+package com.striver.arrays_I;
+
+public class MergedSortedArrays {
+    public static void main(String[] args) {
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int[] nums2 = { 2, 5, 6 };
+        merge(nums1, 3, nums2, 3);
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.print(nums1[i] + " ");
+        }
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+}

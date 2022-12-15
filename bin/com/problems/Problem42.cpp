fcd1f29c310d2cb5f2f39303c882b06e78c0c94a
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-12-15 12:42:17 +0530

    Problem 42 - Rain Water Trapping

---
+++ bin/com/problems/Problem42.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+  
+        int left[n];
+        int right[n];
+        int water = 0;
+
+        left[0] = height[0];
+        for (int i = 1; i < n; i++)
+            left[i] = max(left[i - 1], height[i]);
+
+        right[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+            right[i] = max(right[i + 1], height[i]);
+
+        for (int i = 1; i < n - 1; i++) {
+            int var = min(left[i - 1], right[i + 1]);
+            if (var > height[i]) {
+                water += var - height[i];
+            }
+        }
+
+        return water;
+    }
+};

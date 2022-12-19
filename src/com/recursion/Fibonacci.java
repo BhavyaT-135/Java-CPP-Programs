b376fdb6024e9c1a8d42000b10f55dbc998424b0
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-12-19 16:31:01 +0530

    Recursion Practice

---
+++ src/com/recursion/Fibonacci.java
@@ -0,0 +1,14 @@
+package com.recursion;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+    }
+    
+    public static int fib (int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+}
---
+++ src/com/recursion/NumberExampleRecursion.java
@@ -0,0 +1,16 @@
+package com.recursion;
+
+public class NumberExampleRecursion {
+    public static void main(String[] args) {
+        print(1);
+    }
+    
+    public static void print(int n) {
+        if (n == 5) {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
+    }
+}

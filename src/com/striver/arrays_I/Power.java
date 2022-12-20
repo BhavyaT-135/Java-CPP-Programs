5d9ff0263f8d5a12397a800e4ca483f90b9aa0ec
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-12-20 18:01:50 +0530

    Power - Striver Problem

---
+++ src/com/striver/arrays_I/Power.java
@@ -0,0 +1,29 @@
+package com.striver.arrays_I;
+
+public class Power {
+    public static void main(String[] args) {
+        double x = 2.00000;
+        int n = 10;
+        double ans = myPow(x, n);
+        System.out.println(ans);
+    }
+    
+    public static double myPow(double x, int n) {
+        double ans = 1.0;
+        long nn = n;
+        if (nn < 0)
+            nn = -1 * nn;
+        while (nn > 0) {
+            if (nn % 2 == 1) {
+                ans = ans * x;
+                nn = nn - 1;
+            } else {
+                x = x * x;
+                nn = nn / 2;
+            }
+        }
+        if (n < 0)
+            ans = (double) (1.0) / (double) (ans);
+        return ans;
+    }
+}

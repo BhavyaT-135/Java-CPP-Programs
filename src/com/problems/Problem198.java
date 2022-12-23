56eb7b65312d8466946e67437f9552b0ae39e469
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-12-23 19:45:32 +0530

    Problem 198 - House Robbery

---
+++ src/com/problems/Problem198.java
@@ -0,0 +1,30 @@
+package com.problems;
+
+public class Problem198 {
+    public static void main(String[] args) {
+        int[] arr = { 2, 7, 9, 3, 1 };
+        int ans = rob(arr);
+        System.out.println(ans);
+    }
+    
+    public static int rob(int[] arr) {
+        int n = arr.length;
+        if (n == 0)
+            return 0;
+
+        int v1 = arr[0];
+        if (n == 1)
+            return v1;
+
+        int v2 = Math.max(arr[0], arr[1]);
+        if (n == 2)
+            return v2;
+        int max_val = 0;
+        for (int i = 2; i < n; i++) {
+            max_val = Math.max(arr[i] + v1, v2);
+            v1 = v2;
+            v2 = max_val;
+        }
+        return max_val;
+    }
+}

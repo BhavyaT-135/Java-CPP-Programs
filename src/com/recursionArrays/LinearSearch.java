6df296baad43bb8c6895b95044883bb36c42a596
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-01-08 01:27:53 +0530

    Recursion Linear Search - Returning array list

--- src/com/recursionArrays/LinearSearch.java
+++ src/com/recursionArrays/LinearSearch.java
@@ -9,6 +9,7 @@ public class LinearSearch {
         System.out.println("Index of the target element is " + linearSearch(arr, target, 0));
         linearSearchAll(arr, target, 0);
         System.out.println(list);
+        System.out.println(linearSearchAll2(arr, target, 0));
     }
     
     static int linearSearch(int[] arr, int target, int i) {
@@ -22,6 +23,7 @@ public class LinearSearch {
     }
     
     static ArrayList<Integer> list = new ArrayList<>();
+
     static void linearSearchAll(int[] arr, int target, int i) {
         if (arr[i] == target) {
             list.add(i);
@@ -30,5 +32,25 @@ public class LinearSearch {
             return;
         }
         linearSearchAll(arr, target, ++i);
+    }
+
+    static ArrayList<Integer> linearSearchAll2(int[] arr, int target, int index) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        if (index == arr.length) {
+            return list;
+        }
+
+        // this will contain answer for that function call only
+        if (arr[index] == target) {
+
+            list.add(index);
+        }
+
+        ArrayList<Integer> ansFromBelowCalls = linearSearchAll2(arr, target, index + 1);
+
+        list.addAll(ansFromBelowCalls);
+
+        return list;
     }
 }

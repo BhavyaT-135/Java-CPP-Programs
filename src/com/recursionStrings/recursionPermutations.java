be3fab0a9b4ee04db27ad29fc552e913cd400214
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-02-10 17:10:21 +0530

    String permutations with Array List

--- src/com/recursionStrings/recursionPermutations.java
+++ src/com/recursionStrings/recursionPermutations.java
@@ -1,12 +1,16 @@
 package com.recursionStrings;
 
+import java.util.ArrayList;
+
 public class recursionPermutations {
     public static void main(String[] args) {
         String temp = "abc";
         permutations("", temp);
+        ArrayList<String> ans = permutations2("", temp);
+        System.out.println(ans.toString());
     }
     
-    static void permutations(String p, String up){
+    static void permutations(String p, String up) {
         if (up.isEmpty()) {
             System.out.println(p);
             return;
@@ -18,5 +22,25 @@ public class recursionPermutations {
             String second = p.substring(i, p.length());
             permutations(first + ch + second, up.substring(1));
         }
+    }
+    
+    static ArrayList<String> permutations2(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (int i = 0; i <= p.length(); i++) {
+            String first = p.substring(0, i);
+            String second = p.substring(i, p.length());
+            ans.addAll(permutations2(first + ch + second, up.substring(1)));
+        }
+
+        return ans;
     }
 }

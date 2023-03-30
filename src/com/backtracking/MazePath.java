de737f10ba766cb55220ab9a4a8c903886f6314a
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-03-30 22:24:48 +0530

    Maze Path Problem using Array List

--- src/com/backtracking/MazePath.java
+++ src/com/backtracking/MazePath.java
@@ -1,8 +1,11 @@
 package com.backtracking;
 
+import java.util.ArrayList;
+
 public class MazePath {
   public static void main(String[] args) {
     mazePath("", 3, 3);
+    System.out.println(path("", 3, 3));
   }
   
   static void mazePath(String p, int r, int c) {
@@ -14,7 +17,26 @@ public class MazePath {
       mazePath(p + 'D', r - 1, c);
     }
     if (c > 1) {
-      mazePath(p + 'R', r, c-1);
+      mazePath(p + 'R', r, c - 1);
     }
+  }
+  
+  static ArrayList<String> path(String p, int r, int c) {
+    if (r == 1 && c == 1) {
+      ArrayList<String> list = new ArrayList<>();
+      list.add(p);
+      return list;
+    }
+
+    ArrayList<String> list = new ArrayList<>();
+    
+    if (r > 1) {
+      list.addAll(path(p + 'D', r - 1, c));
+    }
+    if (c > 1) {
+      list.addAll(path(p + 'R', r, c - 1));
+    }
+    
+    return list;
   }
 }

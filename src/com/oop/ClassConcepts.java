6365cb46cdb60327863d5d89ce557fe0ab2793b9
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-09 22:58:11 +0530

    OOP - constructor overloading and class functions

--- src/com/oop/ClassConcepts.java
+++ src/com/oop/ClassConcepts.java
@@ -12,6 +12,20 @@ public class ClassConcepts {
       this.name = " ";
       this.marks = 0;
     }
+
+    Student (int rno, String name, float marks) {
+      this.rno = rno;
+      this.name = name;
+      this.marks = marks;
+    }
+
+    void greeting() {
+      System.out.println("Hi, my name is " + this.name);
+    }
+
+    void changeName(String name) {
+      this.name = name;
+    }
   }
   
   public static void main(String[] args) {
@@ -22,10 +36,18 @@ public class ClassConcepts {
     System.out.println(bhavya.marks);
     System.out.println(bhavya.rno);
     System.out.println(bhavya.name);
+    bhavya.greeting();
 
     Student abhinav = new Student();
     System.out.println(abhinav.marks);
     System.out.println(abhinav.rno);
     System.out.println(abhinav.name);
+    abhinav.greeting();
+
+    Student gaurav = new Student(12, "Gaurav Dubey", 88.75f);
+    System.out.println(gaurav.marks);
+    System.out.println(gaurav.rno);
+    System.out.println(gaurav.name);
+    gaurav.greeting();
   }
 }

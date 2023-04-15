e01e7d35bd0dc90373fd6fd5f9085e711892f6e1
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-15 23:23:43 +0530

    OOP - Packages

---
+++ src/com/oop/packages/a/Greeting.java
@@ -0,0 +1,9 @@
+package com.oop.packages.a;
+import com.oop.packages.b.Message;
+
+public class Greeting {
+  public static void main(String[] args) {
+    System.out.println("This is really cool!");
+    Message.message();
+  }
+}
---
+++ src/com/oop/packages/b/Message.java
@@ -0,0 +1,11 @@
+package com.oop.packages.b;
+
+public class Message {
+  public static void main(String[] args) {
+    System.out.println("Message Class");
+  }
+    
+  public static void message() {
+    System.out.println("Hello World");
+  }
+}

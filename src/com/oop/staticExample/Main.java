0fa21cdfbd606b19f5e8b030b99f14fde920fdc4
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-17 14:14:35 +0530

    OOP - Human class and Main class - same package

---
+++ src/com/oop/staticExample/Main.java
@@ -0,0 +1,8 @@
+package com.oop.staticExample;
+
+public class Main {
+  public static void main(String[] args) {
+    Human bhavya = new Human(21, "Bhavya", 200000, true);
+    System.out.println(bhavya.name);
+  }
+}

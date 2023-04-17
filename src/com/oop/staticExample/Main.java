98aebde7287f8dfd4fb626193dda0ae8a80bf483
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-17 23:03:54 +0530

    Oop - static keyword example

--- src/com/oop/staticExample/Main.java
+++ src/com/oop/staticExample/Main.java
@@ -3,6 +3,9 @@ package com.oop.staticExample;
 public class Main {
   public static void main(String[] args) {
     Human bhavya = new Human(21, "Bhavya", 200000, true);
-    System.out.println(bhavya.name);
+    Human rahul = new Human(21, "Rahul", 200000, true);
+    System.out.println(bhavya.population);
+    System.out.println(rahul.population);
+    System.out.println(Human.population); // Convention to use class name to reference static variables.
   }
 }

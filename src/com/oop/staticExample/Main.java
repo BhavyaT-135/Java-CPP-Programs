c98b0b2b52ece3b2ab38e46b2908b1b25afaaa98
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-20 13:54:08 +0530

    Static, non-static methods

--- src/com/oop/staticExample/Main.java
+++ src/com/oop/staticExample/Main.java
@@ -2,10 +2,21 @@ package com.oop.staticExample;
 
 public class Main {
   public static void main(String[] args) {
-    Human bhavya = new Human(21, "Bhavya", 200000, true);
-    Human rahul = new Human(21, "Rahul", 200000, true);
-    System.out.println(bhavya.population);
-    System.out.println(rahul.population);
-    System.out.println(Human.population); // Convention to use class name to reference static variables.
+    // Human bhavya = new Human(21, "Bhavya", 200000, true);
+    // Human rahul = new Human(21, "Rahul", 200000, true);
+    // System.out.println(bhavya.population);
+    // System.out.println(rahul.population);
+    // System.out.println(Human.population); // Convention to use class name to reference static variables.
+
+    fun();
+  }
+
+  static void fun(){
+    System.out.println("Having fun!");
+  }
+
+  void greeting() {
+    System.out.println("Hello World!");
+    fun();
   }
 }

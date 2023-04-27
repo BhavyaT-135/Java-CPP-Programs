f697c357aa1cd4cc0f1ff30046aec186e773180b
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-27 23:20:18 +0530

    Child class - Parent class - Referencing

--- src/com/oop/inheritance/Main.java
+++ src/com/oop/inheritance/Main.java
@@ -13,8 +13,14 @@ public class Main {
     // System.out.println(box.h);
     // System.out.println(box.w);
     
-    BoxWeight box2 = new BoxWeight(2, 3, 4, 5);
-    box2.information();
-    System.out.println(box2.l + " " + box2.h);
+    // BoxWeight box2 = new BoxWeight(2, 3, 4, 5);
+    // box2.information();
+    // System.out.println(box2.l + " " + box2.h);
+
+    Box box3 = new BoxWeight(2, 3, 4, 5);
+    // It is actually the type of the reference variable and not the type of object that determines what properties can be accessed.
+    // System.out.println(box3.weight); // Thus, this would give an error
+
+    // BoxWeight box4 = new Box(2, 3, 4); // This would also give an error as a child class can't reference a parent class 
   }
 }
